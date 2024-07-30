3d8f2325a7cc3c8f45fc4e2172066e7d38c27df0
seojaeoh <seojaeohcoder@gmail.com>
2024-07-30 12:20:49 +0900

    change_start_activity & add_fade_animation & next_activity

---
+++ android/app/src/main/java/com/example/sidapplication/LoginActivity.java
@@ -0,0 +1,6 @@
+package com.example.sidapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+}
--- android/app/src/main/java/com/example/sidapplication/MainActivity.java
+++ android/app/src/main/java/com/example/sidapplication/MainActivity.java
@@ -13,25 +13,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Animation fadeInAnim;
-    private View mLayout;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-        mLayout = findViewById(R.id.main);
-        fadeInAnim = AnimationUtils.loadAnimation(this, R.anim.fade_in);
-        mLayout.startAnimation(fadeInAnim);
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
-
-
 }
