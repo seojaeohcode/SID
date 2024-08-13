ded75959b279e23ed30041a50cb77aa6a27ab551
seojaeoh <seojaeohcoder@gmail.com>
2024-08-14 06:57:57 +0900

    Gradle build error solved & CameraX preview test success

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,32 +42,38 @@ android {
 
 dependencies {
     // Import the Firebase BoM
-    implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
-
+    implementation(platform(libs.firebase.bom))
 
     // TODO: Add the dependencies for Firebase products you want to use
     // When using the BoM, don't specify versions in Firebase dependencies
-    implementation("com.google.firebase:firebase-analytics")
-
+    implementation(libs.firebase.analytics)
 
     // Add the dependencies for any other desired Firebase products
     // https://firebase.google.com/docs/android/setup#available-libraries
 
     // Add the dependency for the Firebase Authentication library
     // When using the BoM, you don't specify versions in Firebase library dependencies
-    implementation("com.google.firebase:firebase-auth")
+    implementation(libs.firebase.auth)
 
     // Also add the dependency for the Google Play services library and specify its version
-    implementation("com.google.android.gms:play-services-auth:20.7.0")
+    implementation(libs.play.services.auth)
+
+    implementation(libs.camera.core)
+    implementation(libs.camera.camera2)
+    implementation(libs.camera.lifecycle)
+    implementation(libs.camera.video)
+
+    implementation(libs.camera.view)
+    implementation(libs.camera.extensions)
 
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(project(":opencv"))
-    implementation(libs.camera.core)
-    implementation(libs.camera.lifecycle)
-    implementation(libs.camera.view)
+    //implementation(libs.camera.core)
+    //implementation(libs.camera.lifecycle)
+    //implementation(libs.camera.view)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
